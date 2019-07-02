34ebdc2c6bd21a69cd0f02b7d221dc3e5afec988
Jamal Guyo Abgudo <jamalguyo08@gmail.com>
2019-07-02 23:27:46 +0300

    add filter functionality to the products component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
@@ -15,7 +16,7 @@ import { ProductDetailComponent } from './products/product-detail/product-detail
     ProductsComponent,
     ProductDetailComponent
   ],
-  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -10,17 +10,36 @@ import { IProduct } from './product';
 export class ProductsComponent implements OnInit {
   imgMargin: number = 10;
   imgWidth: number = 50;
-  imgStatus: boolean = false;
+  imgStatus: boolean = true;
+  _listFilter: string;
+  get listFilter(): string {
+    return this._listFilter;
+  }
+  set listFilter(value: string) {
+    this._listFilter = value;
+    this.filteredProducts = this.listFilter
+      ? this.performFilter(this.listFilter)
+      : this.products;
+  }
   products: IProduct[];
+  filteredProducts: IProduct[];
   constructor(private productsService: ProductService) {}
 
   ngOnInit() {
-    this.productsService
-      .getProducts()
-      .subscribe(products => (this.products = products));
+    this.productsService.getProducts().subscribe(products => {
+      this.products = products;
+      this.filteredProducts = this.products;
+    });
   }
 
   toggleImage(): void {
     this.imgStatus = !this.imgStatus;
+  }
+  performFilter(filterBy: string): IProduct[] {
+    filterBy = filterBy.toLowerCase();
+    return this.products.filter(
+      (product: IProduct) =>
+        product.productName.toLowerCase().indexOf(filterBy) !== -1
+    );
   }
 }
