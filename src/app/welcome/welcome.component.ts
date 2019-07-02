f71e024048e8b7d30f46d4091f4ee29c25eed8b6
Jamal Guyo Abgudo <jamalguyo08@gmail.com>
2019-07-02 21:54:02 +0300

    create a welcome template content

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./welcome.component.css']
 })
 export class WelcomeComponent implements OnInit {
+  headTitle: string = 'Demo  app';
+  imgMargin: number = 10;
+  imgWidth: number = 350;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
