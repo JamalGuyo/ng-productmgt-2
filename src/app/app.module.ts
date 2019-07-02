668ba35f8083f8fc4223ed1df33d6bc10b2a62e4
Jamal Guyo Abgudo <jamalguyo08@gmail.com>
2019-07-02 22:46:39 +0300

    create a product service and use it in product component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
@@ -14,7 +15,7 @@ import { ProductDetailComponent } from './products/product-detail/product-detail
     ProductsComponent,
     ProductDetailComponent
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,3 +1,4 @@
+import { ProductService } from './product.service';
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 
@@ -10,33 +11,14 @@ export class ProductsComponent implements OnInit {
   imgMargin: number = 10;
   imgWidth: number = 50;
   imgStatus: boolean = false;
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: 'Leaf Rake',
-      productCode: 'GDN-0011',
-      releaseDate: 'March 19, 2016',
-      description: 'Leaf rake with 48-inch wooden handle.',
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl:
-        'https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
-    },
-    {
-      productId: 2,
-      productName: 'Garden Cart',
-      productCode: 'GDN-0023',
-      releaseDate: 'March 18, 2016',
-      description: '15 gallon capacity rolling garden cart',
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl:
-        'https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png'
-    }
-  ];
-  constructor() {}
+  products: IProduct[];
+  constructor(private productsService: ProductService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.productsService
+      .getProducts()
+      .subscribe(products => (this.products = products));
+  }
 
   toggleImage(): void {
     this.imgStatus = !this.imgStatus;
